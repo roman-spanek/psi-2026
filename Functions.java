f73bc055c4d1c2707c0ebb11e360c97fb60997f8
Roman Spanek <38750113+roman-spanek@users.noreply.github.com>
2026-02-24 11:52:35 +0100

    Functions (#1)

---
+++ Functions.java
@@ -0,0 +1,13 @@
+class Functions {
+    private final int LOWEST = 10;
+    private final int HIGHEST = 100;
+    int add(int a, int b) {
+        if (a < LOWEST || a > HIGHEST ||
+            b < LOWEST || b > HIGHEST) {
+            System.out.println("Values must be between 10 and 100");
+            return -1;
+        }
+        return a + b;
+    }
+}
+
